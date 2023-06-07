75e90e6d54487064a499b8acb9d4c8a530a1805d
Bala Chinnaiah <97093618+BalaChinnaiah@users.noreply.github.com>
2023-06-07 16:59:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+// OPERATORS
+
+// Assignment Ops
+let x=10
+let y = 5
+// Arithmetic ops
+console.log(x/y)
+console.log(x+y)
+console.log(x-y)
+console.log(x%y)
+console.log(x*y)
+
+console.log(++x)
+console.log(--y)
+console.log(y--)
+console.log(y)
+
+// Comparision ops
+
+console.log(x==y)
+console.log(x!=y)
+console.log(x===y)
+console.log(x!==y)
+console.log(x>=y)
+console.log(x<=y)
+console.log(x<y)
+console.log(x>y)
+
+// Logical Ops
+const isTrue = x >10 && 8>y 
+console.log(isTrue)
+
+const isFalse = x >10 || 8>y 
+console.log(isFalse)
+console.log(!isFalse)
+
+// Sring ops
+console.log('Bala '+'Sagar')
+
+// ternary ops
+const isNum = 10%2===0 ? 'Is Even nummber' : ' is Odd Number'
+console.log(isNum)
